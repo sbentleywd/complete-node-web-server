4d6367d831fba4c52cd3d3a9a562dacdcd5db564
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2020-12-28 14:09:49 +0000

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,26 +3,22 @@ const searchElement = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-// messageOne.textContent = "Hello";
-
 weatherForm.addEventListener("submit", (event) => {
 	event.preventDefault();
 	const location = searchElement.value;
 
 	messageOne.textContent = "Loading";
 	messageTwo.textContent = "";
-	fetch(`http://localhost:3000/weather?address=${location}`).then(
-		(response) => {
-			if (response.ok) {
-				response.json().then((jsonResponse) => {
-					if (jsonResponse.error) {
-						messageOne.textContent = jsonResponse.error;
-					} else {
-						messageOne.textContent = jsonResponse.location;
-						messageTwo.textContent = jsonResponse.forecast;
-					}
-				});
-			}
+	fetch(`/weather?address=${location}`).then((response) => {
+		if (response.ok) {
+			response.json().then((jsonResponse) => {
+				if (jsonResponse.error) {
+					messageOne.textContent = jsonResponse.error;
+				} else {
+					messageOne.textContent = jsonResponse.location;
+					messageTwo.textContent = jsonResponse.forecast;
+				}
+			});
 		}
-	);
+	});
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicPath = path.join(__dirname, "../public");
@@ -98,6 +99,6 @@ app.get("*", (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log("Server listening on port 3000");
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
 });
